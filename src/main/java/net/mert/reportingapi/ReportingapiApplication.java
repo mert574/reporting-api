2449595dbc760ce010a078602dcc6ad58ebc0870
Mert <mert574@users.noreply.github.com>
2018-11-27 13:42:31 +0300

    Initialize maven project
    Spring boot

---
+++ src/main/java/net/mert/reportingapi/ReportingapiApplication.java
@@ -0,0 +1,12 @@
+package net.mert.reportingapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReportingapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReportingapiApplication.class, args);
+    }
+}
