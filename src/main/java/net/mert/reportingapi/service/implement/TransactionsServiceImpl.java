9bfc1fdeb8fbe399108269a36c82c180edb9eba8
Mert <mert574@users.noreply.github.com>
2018-11-28 15:54:17 +0300

    Bump: Implement API Consuming in /transactions/report
    Forgot to commit this file.

--- src/main/java/net/mert/reportingapi/service/implement/TransactionsServiceImpl.java
+++ src/main/java/net/mert/reportingapi/service/implement/TransactionsServiceImpl.java
@@ -1,36 +1,39 @@
 package net.mert.reportingapi.service.implement;
 
-import net.mert.reportingapi.model.TransactionReport;
+import net.mert.reportingapi.model.request.TransactionsReportRequest;
+import net.mert.reportingapi.model.response.ErrorResponse;
+import net.mert.reportingapi.model.response.ResponseTemplate;
+import net.mert.reportingapi.model.response.TokenResponse;
 import net.mert.reportingapi.model.response.TransactionsReportResponse;
 import net.mert.reportingapi.service.TransactionsService;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
 @Service
 public class TransactionsServiceImpl implements TransactionsService {
 
+    private final RestTemplate restTemplate = new RestTemplate();
+
     @Override
-    public Optional<TransactionsReportResponse> queryReport(String fromDate, String toDate,
-                                                            int merchant, int acquirer) {
-        if (fromDate.isEmpty() && toDate.isEmpty()) {
-            return Optional.empty();
+    public Optional<ResponseTemplate> queryReport(TransactionsReportRequest request, TokenResponse token) {
+        HttpHeaders tokenHeader = new HttpHeaders();
+        tokenHeader.set("Authorization", token.getToken());
+
+        try {
+            ResponseEntity<TransactionsReportResponse> response = restTemplate.postForEntity(
+                    "https://sandbox-reporting.rpdpymnt.com/api/v3/transactions/report",
+                    new HttpEntity<>(request, tokenHeader),
+                    TransactionsReportResponse.class);
+
+            return Optional.ofNullable(response.getBody());
+
+        } catch (Exception exception) {
+            return Optional.of(new ErrorResponse(exception.getMessage(), "DECLINED"));
         }
-
-        // TODO: Consume the API somehow and get the results.
-        TransactionsReportResponse resp = new TransactionsReportResponse();
-
-        // Create mock data...
-        List<TransactionReport> list = new ArrayList<>();
-        for (int i = 0; i < 5; i++) {
-            TransactionReport newTransactionReport = new TransactionReport(i * 100, i * 534, i % 3 == 0 ? "USD" : "EUR");
-            list.add(newTransactionReport);
-        }
-
-        resp.setTransactionReports(list);
-
-        return Optional.of(resp);
     }
 }
