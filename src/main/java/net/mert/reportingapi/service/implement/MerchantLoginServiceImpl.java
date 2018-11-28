e134e26d91d5133808c27cf194ff83cf97b4084d
Mert <mert574@users.noreply.github.com>
2018-11-28 15:55:11 +0300

    Update MerchantLoginServiceImpl.java
    Updated it to be more consistent

--- src/main/java/net/mert/reportingapi/service/implement/MerchantLoginServiceImpl.java
+++ src/main/java/net/mert/reportingapi/service/implement/MerchantLoginServiceImpl.java
@@ -1,12 +1,12 @@
 package net.mert.reportingapi.service.implement;
 
+import net.mert.reportingapi.model.request.MerchantLoginRequest;
 import net.mert.reportingapi.model.response.TokenResponse;
 import net.mert.reportingapi.service.MerchantLoginService;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.util.LinkedMultiValueMap;
-import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Optional;
@@ -17,15 +17,13 @@ public class MerchantLoginServiceImpl implements MerchantLoginService {
     private final RestTemplate restTemplate = new RestTemplate();
 
     @Override
-    public Optional<TokenResponse> login(String email, String password) {
-
-        MultiValueMap<String, String> parts = new LinkedMultiValueMap<>(2);
-        parts.add("email", email);
-        parts.add("password", password);
+    public Optional<TokenResponse> login(MerchantLoginRequest request) {
 
         try {
             ResponseEntity<TokenResponse> response = restTemplate.postForEntity(
-                    "https://sandbox-reporting.rpdpymnt.com/api/v3/merchant/user/login", parts, TokenResponse.class);
+                    "https://sandbox-reporting.rpdpymnt.com/api/v3/merchant/user/login",
+                    new HttpEntity<>(request),
+                    TokenResponse.class);
 
             if (response.getStatusCode() == HttpStatus.OK && response.getBody() != null) {
                 return Optional.of(response.getBody());
