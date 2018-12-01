097fd70a79da50385320348b8620cc0419b79cc8
Mert <mert574@users.noreply.github.com>
2018-12-01 23:54:26 +0300

    Merge with frontend
    Added first build version of https://github.com/mert574/report-api-frontend

---
+++ src/main/resources/static/precache-manifest.7998c5395b37bc7244518bb7d04b0ad7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2a710aee3d53efa4f826",
+    "url": "/static/js/main.2a710aee.chunk.js"
+  },
+  {
+    "revision": "24e59b05d5e78b2b9248",
+    "url": "/static/js/1.24e59b05.chunk.js"
+  },
+  {
+    "revision": "2a710aee3d53efa4f826",
+    "url": "/static/css/main.5c21fbc8.chunk.css"
+  },
+  {
+    "revision": "24e59b05d5e78b2b9248",
+    "url": "/static/css/1.8d0c3bac.chunk.css"
+  },
+  {
+    "revision": "3b7d972db2805c4879ef432a59ddb0d9",
+    "url": "/index.html"
+  }
+];
